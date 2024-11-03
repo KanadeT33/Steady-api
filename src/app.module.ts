b2310da70c0a5e54aeafca7ec73a00f4be94ca4b
Marcelo Hernández <mhernandez@dhemax.com>
2024-11-03 18:20:52 -0300

    Add POST endpoint for user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UserModule } from './resources/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',         // Cambia según tu configuración
+    port: 5432,                // Puerto de PostgreSQL
+    username: 'postgres',    // Usuario de la base de datos
+    password: '208', // Contraseña de la base de datos
+    database: 'SteadyDB',         // Nombre de la base de datos
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,         // Solo para desarrollo; en producción, usa migraciones
+  }),UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/resources/user/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ){}
+  async create(createUserDto: CreateUserDto) {
+    const {username, email, password}= createUserDto
+    const newUser= this.userRepository.create({
+      username,
+      password,
+      email
+    })
+    const savedUSer= await this.userRepository.save(newUser)
+    if (savedUSer) {
+      return  `Success creating user ${savedUSer}`
+    }
+    return 'no'
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
